9b66ffd5985f33b7c83ec40b7cfe6747f029c43c
sahil1418 <sahil0329308@gmail.com>
2026-03-25 02:46:29 +0530

    Update vite.config.js

--- complaintos/vite.config.js
+++ complaintos/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  base: './', // Use relative paths for built assets
+  plugins: [react()],
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve('./src'),
